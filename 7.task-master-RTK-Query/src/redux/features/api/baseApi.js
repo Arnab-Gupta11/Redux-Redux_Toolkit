722ef2899a2dc7526025888dcb280c7b55dec5c8
Arnab Gupta <arnabgupta1111@gmail.com>
2024-06-14 23:42:12 +0600

    7.task-master-RTK-Query: update baseApi

--- 7.task-master-RTK-Query/src/redux/features/api/baseApi.js
+++ 7.task-master-RTK-Query/src/redux/features/api/baseApi.js
@@ -3,27 +3,6 @@ export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
   tagTypes: ["Tasks"],
-  endpoints: (builder) => ({
-    getTasks: builder.query({
-      query: () => "/tasks",
-      providesTags: ["Tasks"],
-    }),
-    updateTasksStatus: builder.mutation({
-      query: ({ id, data }) => ({
-        url: `/tasks/${id}`,
-        method: "PATCH",
-        body: data,
-      }),
-      invalidatesTags: ["Tasks"],
-    }),
-    addTasks: builder.mutation({
-      query: (tasks) => ({
-        url: "/tasks",
-        method: "POST",
-        body: tasks,
-      }),
-      invalidatesTags: ["Tasks"],
-    }),
-  }),
+  endpoints: () => ({}),
 });
 export const { useGetTasksQuery, useUpdateTasksStatusMutation, useAddTasksMutation } = baseApi;
